23da0f4caff4f3a85968a18f279ba75bfcd1b7da
jamalihassan0307 <jamalihassan0307@gmail.com>
2025-03-11 09:14:39 +0500

    Add Sofas to TabBarMenu

--- lib/data/tab_bar_menu.dart
+++ lib/data/tab_bar_menu.dart
@@ -5,4 +5,5 @@ List<TabBarMenuButton> tabBarMenu = [
   TabBarMenuButton(id: 1, name: 'Chairs'),
   TabBarMenuButton(id: 2, name: 'Lamps'),
   TabBarMenuButton(id: 3, name: 'Tables'),
+  TabBarMenuButton(id: 4, name: 'Sofas'),
 ];
